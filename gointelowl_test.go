7ae3b64a6596febdd4ba72852a154f687278ed0d
spiderxm <mrigank.rkee@rediffmail.com>
2021-02-11 15:06:51 +0530

    Refactoring Tests

--- gointelowl_test.go
+++ gointelowl_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func TestIntelOwlClient(t *testing.T) {
-	Token := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+	Token := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ"
 	URL := "http://localhost:8080"
 	client := IntelOwlClient{
 		Token:       Token,
