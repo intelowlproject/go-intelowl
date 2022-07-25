257680317d89984491a227ed5cbef1defea3dec9
fear-the-reaper <jta.reaper@gmail.com>
2022-07-25 21:02:44 +0500

    Adding needed endpoints with their unit tests!

--- gointelowl/me.go
+++ gointelowl/me.go
@@ -1,9 +1,12 @@
 package gointelowl
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
+	"net/http"
+	"time"
 )
 
 type Details struct {
@@ -28,6 +31,44 @@ type UserService struct {
 	client *IntelOwlClient
 }
 
+type Owner struct {
+	Username string    `json:"username"`
+	FullName string    `json:"full_name"`
+	Joined   time.Time `json:"joined"`
+}
+
+type Organization struct {
+	MembersCount int        `json:"members_count"`
+	Owner        Owner      `json:"owner"`
+	IsUserOwner  bool       `json:"is_user_owner,omitempty"`
+	CreatedAt    *time.Time `json:"created_at,omitempty"`
+	Name         string     `json:"name"`
+}
+
+type OrganizationParams struct {
+	Name string `json:"name"`
+}
+
+type MemberParams struct {
+	Username string `json:"username"`
+}
+
+type Invite struct {
+	Id        int       `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	Status    string    `json:"status"`
+}
+
+type Invitation struct {
+	Invite
+	Organization Organization `json:"organization"`
+}
+
+type InvitationParams struct {
+	Organization OrganizationParams `json:"organization"`
+	Status       string             `json:"status"`
+}
+
 /*
 * Desc: Get User Details
 * Endpoint: GET /api/me/access
@@ -49,4 +90,118 @@ func (userService *UserService) Access(ctx context.Context) (*User, error) {
 		return nil, unmarshalError
 	}
 	return &user, nil
+}
+
+/*
+* Desc: Get Organization details!
+* Endpoint: GET /api/me/organization
+ */
+func (userService *UserService) Organization(ctx context.Context) (*Organization, error) {
+	requestUrl := fmt.Sprintf("%s/api/me/organization", userService.client.options.Url)
+	contentType := "application/json"
+	method := "GET"
+	request, err := userService.client.buildRequest(ctx, method, contentType, nil, requestUrl)
+	if err != nil {
+		return nil, err
+	}
+
+	org := Organization{}
+	successResp, err := userService.client.newRequest(ctx, request)
+	if err != nil {
+		return nil, err
+	}
+	if unmarshalError := json.Unmarshal(successResp.Data, &org); unmarshalError != nil {
+		return nil, unmarshalError
+	}
+	return &org, nil
+}
+
+/*
+* Desc: Create a super cool organization!
+* Endpoint: POST /api/me/organization
+ */
+func (userService *UserService) CreateOrganization(ctx context.Context, organizationParams *OrganizationParams) (*Organization, error) {
+	requestUrl := fmt.Sprintf("%s/api/me/organization", userService.client.options.Url)
+	// Getting the relevant JSON data
+	orgJson, err := json.Marshal(organizationParams)
+	if err != nil {
+		return nil, err
+	}
+	contentType := "application/json"
+	method := "POST"
+	body := bytes.NewBuffer(orgJson)
+	request, err := userService.client.buildRequest(ctx, method, contentType, body, requestUrl)
+	if err != nil {
+		return nil, err
+	}
+
+	org := Organization{}
+	successResp, err := userService.client.newRequest(ctx, request)
+	if err != nil {
+		return nil, err
+	}
+	if unmarshalError := json.Unmarshal(successResp.Data, &org); unmarshalError != nil {
+		return nil, unmarshalError
+	}
+	return &org, nil
+}
+
+/*
+* Desc: Invite someone to your super cool Organization!
+* Endpoint: POST /api/me/organization/invite
+ */
+func (userService *UserService) InviteToOrganization(ctx context.Context, memberParams *MemberParams) (*Invite, error) {
+	requestUrl := fmt.Sprintf("%s/api/me/organization/invite", userService.client.options.Url)
+	// Getting the relevant JSON data
+	memberJson, err := json.Marshal(memberParams)
+	if err != nil {
+		return nil, err
+	}
+	contentType := "application/json"
+	method := "POST"
+	body := bytes.NewBuffer(memberJson)
+	request, err := userService.client.buildRequest(ctx, method, contentType, body, requestUrl)
+	if err != nil {
+		return nil, err
+	}
+
+	invite := Invite{}
+	successResp, err := userService.client.newRequest(ctx, request)
+	if err != nil {
+		return nil, err
+	}
+	if unmarshalError := json.Unmarshal(successResp.Data, &invite); unmarshalError != nil {
+		return nil, unmarshalError
+	}
+	return &invite, nil
+}
+
+/*
+* Desc: Remove someone from your super cool Organization! (you had your reasons)
+* Endpoint: POST /api/me/organization/remove_member
+ */
+func (userService *UserService) RemoveMemberFromOrganization(ctx context.Context, memberParams *MemberParams) (bool, error) {
+	requestUrl := fmt.Sprintf("%s/api/me/organization/remove_member", userService.client.options.Url)
+	// Getting the relevant JSON data
+	memberJson, err := json.Marshal(memberParams)
+	if err != nil {
+		return false, err
+	}
+	contentType := "application/json"
+	method := "POST"
+	body := bytes.NewBuffer(memberJson)
+	request, err := userService.client.buildRequest(ctx, method, contentType, body, requestUrl)
+	if err != nil {
+		return false, err
+	}
+
+	successResp, err := userService.client.newRequest(ctx, request)
+	if err != nil {
+		return false, err
+	}
+
+	if successResp.StatusCode == http.StatusNoContent {
+		return true, nil
+	}
+	return false, nil
 }
--- tests/userService_test.go
+++ tests/userService_test.go
@@ -47,3 +47,163 @@ func TestUserServiceAccess(t *testing.T) {
 		})
 	}
 }
+
+func TestUserServiceOrganization(t *testing.T) {
+	orgRespJsonStr := `{"members_count":1,"owner":{"username":"hussain","full_name":"h k","joined":"2022-07-23T09:11:08.674294Z"},"is_user_owner":true,"created_at":"2022-07-23T09:11:08.580533Z","name":"StrawHats"}`
+	orgResponse := gointelowl.Organization{}
+	if unmarshalError := json.Unmarshal([]byte(orgRespJsonStr), &orgResponse); unmarshalError != nil {
+		t.Fatalf("Error: %s", unmarshalError)
+	}
+	// *table test case
+	testCases := make(map[string]TestData)
+	testCases["simple"] = TestData{
+		Input:      nil,
+		Data:       orgRespJsonStr,
+		StatusCode: http.StatusOK,
+		Want:       orgResponse,
+	}
+	for name, testCase := range testCases {
+		t.Run(name, func(t *testing.T) {
+			testServer := NewTestServer(&testCase)
+			defer testServer.Close()
+			client := NewTestIntelOwlClient(testServer.URL)
+			ctx := context.Background()
+			gottenOrgResponse, err := client.UserService.Organization(ctx)
+			if testCase.StatusCode < http.StatusOK || testCase.StatusCode >= http.StatusBadRequest {
+				diff := cmp.Diff(testCase.Want, err, cmpopts.IgnoreFields(gointelowl.IntelOwlError{}, "Response"))
+				if diff != "" {
+					t.Fatalf(diff)
+				}
+			} else {
+				diff := cmp.Diff(testCase.Want, (*gottenOrgResponse))
+				if diff != "" {
+					t.Fatalf(diff)
+				}
+			}
+		})
+	}
+}
+
+func TestUserServiceCreateOrganization(t *testing.T) {
+	orgRespJsonStr := `{"members_count":1,"owner":{"username":"notHussain","full_name":"noy Hussain","joined":"2022-07-24T17:34:55.032629Z"},"is_user_owner":true,"created_at":"2022-07-24T17:34:54.971735Z","name":"TestOrganization"}`
+	orgResponse := gointelowl.Organization{}
+	if unmarshalError := json.Unmarshal([]byte(orgRespJsonStr), &orgResponse); unmarshalError != nil {
+		t.Fatalf("Error: %s", unmarshalError)
+	}
+	orgParams := gointelowl.OrganizationParams{
+		Name: "TestOrganization",
+	}
+	testCases := make(map[string]TestData)
+	testCases["simple"] = TestData{
+		Input:      orgParams,
+		Data:       orgRespJsonStr,
+		StatusCode: http.StatusOK,
+		Want:       orgResponse,
+	}
+	for name, testCase := range testCases {
+		t.Run(name, func(t *testing.T) {
+			testServer := NewTestServer(&testCase)
+			defer testServer.Close()
+			client := NewTestIntelOwlClient(testServer.URL)
+			ctx := context.Background()
+			params, ok := testCase.Input.(gointelowl.OrganizationParams)
+			if ok {
+				gottenOrgResponse, err := client.UserService.CreateOrganization(ctx, &params)
+				if testCase.StatusCode < http.StatusOK || testCase.StatusCode >= http.StatusBadRequest {
+					diff := cmp.Diff(testCase.Want, err, cmpopts.IgnoreFields(gointelowl.IntelOwlError{}, "Response"))
+					if diff != "" {
+						t.Fatalf(diff)
+					}
+				} else {
+					diff := cmp.Diff(testCase.Want, (*gottenOrgResponse))
+					if diff != "" {
+						t.Fatalf(diff)
+					}
+				}
+			} else {
+				t.Fatalf("Casting failed!")
+			}
+		})
+	}
+}
+
+func TestUserServiceRemoveMemberFromOrganization(t *testing.T) {
+	memberParams := gointelowl.MemberParams{
+		Username: "TestUser",
+	}
+	testCases := make(map[string]TestData)
+	testCases["simple"] = TestData{
+		Input:      memberParams,
+		Data:       ``,
+		StatusCode: http.StatusNoContent,
+		Want:       true,
+	}
+	for name, testCase := range testCases {
+		t.Run(name, func(t *testing.T) {
+			testServer := NewTestServer(&testCase)
+			defer testServer.Close()
+			client := NewTestIntelOwlClient(testServer.URL)
+			ctx := context.Background()
+			params, ok := testCase.Input.(gointelowl.MemberParams)
+			if ok {
+				left, err := client.UserService.RemoveMemberFromOrganization(ctx, &params)
+				if testCase.StatusCode < http.StatusOK || testCase.StatusCode >= http.StatusBadRequest {
+					diff := cmp.Diff(testCase.Want, err, cmpopts.IgnoreFields(gointelowl.IntelOwlError{}, "Response"))
+					if diff != "" {
+						t.Fatalf(diff)
+					}
+				} else {
+					diff := cmp.Diff(testCase.Want, left)
+					if diff != "" {
+						t.Fatalf(diff)
+					}
+				}
+			} else {
+				t.Fatalf("Casting failed!")
+			}
+		})
+	}
+}
+
+func TestUserServiceInviteToOrganization(t *testing.T) {
+	inviteJsonStr := `{"id":12,"created_at":"2022-07-24T18:43:42.299318Z","status":"pending"}`
+	inviteResponse := gointelowl.Invite{}
+	if unmarshalError := json.Unmarshal([]byte(inviteJsonStr), &inviteResponse); unmarshalError != nil {
+		t.Fatalf("Error: %s", unmarshalError)
+	}
+	memberParams := gointelowl.MemberParams{
+		Username: "TestUser",
+	}
+	testCases := make(map[string]TestData)
+	testCases["simple"] = TestData{
+		Input:      memberParams,
+		Data:       inviteJsonStr,
+		StatusCode: http.StatusCreated,
+		Want:       inviteResponse,
+	}
+	for name, testCase := range testCases {
+		t.Run(name, func(t *testing.T) {
+			testServer := NewTestServer(&testCase)
+			defer testServer.Close()
+			client := NewTestIntelOwlClient(testServer.URL)
+			ctx := context.Background()
+			params, ok := testCase.Input.(gointelowl.MemberParams)
+			if ok {
+				gottenInviteResponse, err := client.UserService.InviteToOrganization(ctx, &params)
+				if testCase.StatusCode < http.StatusOK || testCase.StatusCode >= http.StatusBadRequest {
+					diff := cmp.Diff(testCase.Want, err, cmpopts.IgnoreFields(gointelowl.IntelOwlError{}, "Response"))
+					if diff != "" {
+						t.Fatalf(diff)
+					}
+				} else {
+					diff := cmp.Diff(testCase.Want, (*gottenInviteResponse))
+					if diff != "" {
+						t.Fatalf(diff)
+					}
+				}
+			} else {
+				t.Fatalf("Casting failed!")
+			}
+		})
+	}
+}
